0ac1945e2615391e1ae56bc9a5d703019fddc3db
Julian <me@julian.pro>
2020-12-11 17:00:32 +0100

    first commit

---
+++ flag.go
@@ -0,0 +1,72 @@
+package flag
+
+import (
+	"time"
+
+	cmdx "github.com/jkgx/cmd"
+
+	"github.com/spf13/cobra"
+)
+
+// MustGetBool returns a bool flag or fatals if an error occurs.
+func MustGetBool(cmd *cobra.Command, name string) bool {
+	ok, err := cmd.Flags().GetBool(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return ok
+}
+
+// MustGetString returns a string flag or fatals if an error occurs.
+func MustGetString(cmd *cobra.Command, name string) string {
+	s, err := cmd.Flags().GetString(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return s
+}
+
+// MustGetDuration returns a time.Duration flag or fatals if an error occurs.
+func MustGetDuration(cmd *cobra.Command, name string) time.Duration {
+	d, err := cmd.Flags().GetDuration(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return d
+}
+
+// MustGetStringSlice returns a []string flag or fatals if an error occurs.
+func MustGetStringSlice(cmd *cobra.Command, name string) []string {
+	ss, err := cmd.Flags().GetStringSlice(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return ss
+}
+
+// MustGetInt returns a int flag or fatals if an error occurs.
+func MustGetInt(cmd *cobra.Command, name string) int {
+	ss, err := cmd.Flags().GetInt(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return ss
+}
+
+// MustGetUint8 returns a uint8 flag or fatals if an error occurs.
+func MustGetUint8(cmd *cobra.Command, name string) uint8 {
+	v, err := cmd.Flags().GetUint8(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return v
+}
+
+// MustGetUint32 returns a uint32 flag or fatals if an error occurs.
+func MustGetUint32(cmd *cobra.Command, name string) uint32 {
+	v, err := cmd.Flags().GetUint32(name)
+	if err != nil {
+		cmdx.Fatalf(err.Error())
+	}
+	return v
+}
